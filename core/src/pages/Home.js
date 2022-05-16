947739a696c8756f0aeb65dc47086ba6425e61bf
Sergio Guzmán <82333906+DnaCyphers@users.noreply.github.com>
2022-05-16 18:57:44 -0400

    Make a basic home site

---
+++ core/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {useState, useEffect} from 'react';
+
+function Home() {
+
+  return (
+    <div className="home">
+        <div className="headerContainer">
+          <h1>Password Manager - Sergio Guzman</h1>
+          <h3>SAVE ANYTHING YOU WANT</h3>     
+        </div>
+    </div>
+  )
+}
+
+export default Home
