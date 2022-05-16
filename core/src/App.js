37fdd3c0f7ea52e66750dfb3b840afade17511d1
Sergio Guzmán <82333906+DnaCyphers@users.noreply.github.com>
2022-05-16 18:56:24 -0400

    Navbar links & routes

--- core/src/App.js
+++ core/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Folder from './components/Folder';
+import Items from './components/Items'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/folder" element={<Folder />} />
+        <Route path="/item" element={<Items />} />
+      </Routes>
+    </Router>   
     </div>
   );
 }
